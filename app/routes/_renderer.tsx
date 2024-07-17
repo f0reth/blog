19735e4173f896e7d48ed1a1ed519bad055d4868
f0reth <65765140+f0reth@users.noreply.github.com>
2024-07-18 08:45:25 +0900

    fix: google fonts link

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -50,7 +50,7 @@ export default jsxRenderer(({ children, title, desc, slug }) => {
 				/>
 				<link
 					rel="stylesheet"
-					href="{href}"
+					href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap"
 					media="print"
 					onload='this.media="all"'
 				/>
