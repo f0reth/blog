20685bb84fafea554ab56c3ce294b3ed45dc3f5f
f0reth <65765140+f0reth@users.noreply.github.com>
2024-08-17 16:56:38 +0900

    fix: article path

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -17,7 +17,7 @@ export default createRoute((c) => {
 					>
 						<div class={"flex flex-col gap-1.5 md:gap-3 md:py-3 md:pr-5"}>
 							<a
-								href={`/posts/${post.slug}`}
+								href={`/blog/${post.slug}`}
 								class={
 									"w-fit text-xl md:text-3xl font-bold duration-200 hover:text-primary before:w-1 before:h-5 before:rounded-md before:bg-primary before:absolute before:top-[35px] before:left-[18px] before:hidden md:before:block"
 								}
