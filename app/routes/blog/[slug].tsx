3fcb04904d326368621e10044ad8e9a608bc570b
f0reth <65765140+f0reth@users.noreply.github.com>
2024-08-17 14:54:16 +0900

    change: blog path

---
+++ app/routes/blog/[slug].tsx
@@ -0,0 +1,48 @@
+import Publish from "app/components/published";
+import { getPostBySlug, getPosts } from "app/lib/posts";
+import { ssgParams } from "hono/ssg";
+import { createRoute } from "honox/factory";
+
+export default createRoute(
+	ssgParams(() => {
+		const posts = getPosts();
+		return posts.map((post) => ({
+			slug: post.slug,
+		}));
+	}),
+	(c) => {
+		const slug = c.req.param("slug");
+		if (slug === ":slug") {
+			return c.notFound();
+		}
+
+		const post = getPostBySlug(slug);
+		const title = post?.title ?? "";
+		const desc = post?.description;
+
+		return c.render(
+			<article
+				class={
+					"bg-card rounded-[16px] px-6 md:px-9 pt-6 pb-4 flex flex-col gap-4"
+				}
+			>
+				<div class={"relative"}>
+					<h1
+						class={
+							"text-2xl md:text-4xl font-bold md:before:w-1 md:before:h-5 md:before:rounded-md md:before:bg-primary md:before:absolute md:before:top-[0.75rem] md:before:left-[-1.125rem]"
+						}
+					>
+						{post?.title}
+					</h1>
+				</div>
+				<Publish published={post?.published} modified={post?.modified} />
+				<div class={"markdown"}>{post?.Component({})}</div>
+			</article>,
+			{
+				title,
+				desc,
+				slug,
+			},
+		);
+	},
+);
